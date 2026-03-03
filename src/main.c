bad119167cc73e44154ccfe1ce4220cbf0307eef
Senko <55474532-Vaness17@users.noreply.replit.com>
2026-03-03 11:30:59 +0000

    Function of count n

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
-#include<stdio.h>
-void main(void){
-  printf("Hello World\n");
-  printf("GG");
+#include <stdio.h>
+#include <string.h>
+
+int countLetterN(const char *sentence) {
+    int count = 0;
+    for (int i = 0; sentence[i] != '\0'; i++) {
+        if (sentence[i] == 'н' || sentence[i] == 'Н') {
+            count++;
+        }
+    }
+    return count;
 }
+
